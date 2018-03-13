8b3c7814e2d77e46a264729639c43a2cb397bb87
JinhaoPlus <rowin2013@gmail.com>
2018-03-14 00:35:15 +0800

    Chap3 外部属性注入

--- Chap3_AdvancedWiringBean_injectvalue/src/main/java/top/jinhaoplus/config/PlaceholderDiscConfig.java
+++ Chap3_AdvancedWiringBean_injectvalue/src/main/java/top/jinhaoplus/config/PlaceholderDiscConfig.java
@@ -12,6 +12,7 @@ import top.jinhaoplus.model.AnotherBlankDisc;
 @ComponentScan(basePackageClasses = {AnotherBlankDisc.class})
 public class PlaceholderDiscConfig {
 
+    // PropertyPlaceholderConfigurer这一BeanPostProcessor后续装配的过程中根据其指定的外部配置装配到bean中去
     @Bean
     public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
         // 和placeholder-config.xml中的配置方法实际上是相同的
